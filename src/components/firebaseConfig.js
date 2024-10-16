f9097ec01a6d3dad6c0592132ad8de23110499db
HackDoge29 <teusday436326@gmail.com>
2024-10-16 01:06:53 +0100

    updated

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1J6fXXnD-jR3nSPVHdUkQnt9NTfDYpns",
-  authDomain: "fancamp436326.firebaseapp.com",
-  projectId: "fancamp436326",
-  storageBucket: "fancamp436326.appspot.com",
-  messagingSenderId: "1040066115816",
-  appId: "1:1040066115816:web:99c6b4061022465536715d"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "f",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
